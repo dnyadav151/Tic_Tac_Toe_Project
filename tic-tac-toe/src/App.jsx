b9044eab7fb683b31e6065eb9684d2338adaaa02
dhruv151 <dnyadav209@gmail.com>
2023-08-27 20:00:11 +0530

    Basic tic-tac-toe Setup Done

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Grid from "./Components/Grid/Grid";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <Grid numberOfCards={9} />
+    </>
+  )
+}
+
+export default App;
